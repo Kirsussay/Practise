7bb7065bcdf66b787b29d351fef7c6f51f73d9f9
Kirsussay <3262816046@qq.com>
2025-02-06 20:49:09 +0800

    Third

---
+++ 区间链表反转.py
@@ -0,0 +1,26 @@
+class ListNode:
+    def __init__(self,x):
+       self.val = x
+       self.next = None
+class solution:
+    def reverseBetween(self , head: ListNode, m: int, n: int) -> ListNode:
+        res = ListNode(-1)
+        res.next = head
+        pre = res
+        cur = head
+        i : int = 1
+        while i < m:
+            pre = cur
+            cur = cur.next
+            i = 1 + i
+        j : int = m
+        while j < n:
+            temp = cur.next
+            cur.next = temp.next
+            temp.next = pre.next
+            pre.next = temp
+        return res.next
+
+        
+
+        
