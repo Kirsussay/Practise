0a63b4024a3c445a058272bbdc1bdef2a69e81ee
Kirsussay <3262816046@qq.com>
2025-02-06 21:10:22 +0800

    add algorithm principle

--- 区间链表反转.py
+++ 区间链表反转.py
@@ -8,11 +8,13 @@ class solution:
         res.next = head
         pre = res
         cur = head
+        #循环找到区间左起点
         i : int = 1
         while i < m:
             pre = cur
             cur = cur.next
             i = 1 + i
+        #算法实现
         j : int = m
         while j < n:
             temp = cur.next
@@ -21,7 +23,31 @@ class solution:
             pre.next = temp
             j = j+1
         return res.next
+    
+# 辅助函数：打印链表
+def printList(head):
+    while head:
+        print(head.val, end=" -> ")
+        head = head.next
+    print("None")
+        # 测试用例
+if __name__ == "__main__":
+    # 创建链表 1 -> 2 -> 3 -> 4 -> 5
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
 
-        
+    # 打印原始链表
+    print("原始链表：")
+    printList(head)
 
-        
+    # 调用方法反转区间 [2, 4]
+    solution = solution()
+    new_head = solution.reverseBetween(head, 2, 4)
+
+    # 打印反转后的链表
+    print("反转区间 [2, 4] 后的链表：")
+    printList(new_head)
+     
--- 反转链表.py
+++ 反转链表.py
@@ -15,4 +15,29 @@ class Solution:
         return prev
 
 
+# 辅助函数：打印链表
+def printList(head):
+    while head:
+        print(head.val, end=" -> ")
+        head = head.next
+    print("None")
+        # 测试用例
+if __name__ == "__main__":
+    # 创建链表 1 -> 2 -> 3 -> 4 -> 5
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
 
+    # 打印原始链表
+    print("原始链表：")
+    printList(head)
+
+    # 调用方法反转区间 [2, 4]
+    solution = Solution()
+    new_head = solution.ReverseList(head)
+
+    # 打印反转后的链表
+    print("反转区间 [2, 4] 后的链表：")
+    printList(new_head)
